9b3076f1a17cf57c3ea6fd1bc6574798cfdd7b4e
isaiassm <isaias1113@outlook.com>
2018-09-05 21:18:31 -0300

    Inicio de JavaScript, com criaçao de variaveis, let e constantes

---
+++ fundamentos/comentario.js
@@ -0,0 +1,6 @@
+console.log('teste')
+
+/*
+* comentario de multiplas linhas
+*
+*/
---
+++ fundamentos/variaveisConstantes.js
@@ -0,0 +1,23 @@
+var a = 3 
+
+//forma de criar variavel
+let b = 4
+
+var a = 30
+b = 40
+
+//não se pode redeclarar variavel com let
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+//constante não pode ter o valor renomeado
+const c = 5 
+//c = 50
+console.log(c)
+
+
