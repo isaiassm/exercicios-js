c36d94b1d0469b92b7ea308e024f40329e1837c3
isaiassm <isaias1113@outlook.com>
2018-09-05 21:31:46 -0300

    aula para exemplificar tipagem fraca da linguagem

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nomes genericos e siglas
+let valor = ''
+let numero = 1
+let pqp = false //Produto quimico perigoso... kkk
+
+//não faz sentido
