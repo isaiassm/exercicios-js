af860388d1f0499fee01dfecc2c91e0e62dd905c
isaiassm <isaias1113@outlook.com>
2018-09-11 16:45:00 -0300

    entendo a estrutura e ultilizacao de objetos
    entendendo o tratamento de null e undefined

---
+++ fundamentos/objeto.js
@@ -0,0 +1,17 @@
+//chaves significa criacao de objeto
+const prod1 = {}
+prod1.nome = 'celular ultra mega'
+prod1.preco = 4998.90
+prod1 ['desconto legal'] = 0.40 //evitar atributos com espaço
+
+console.log(prod1)
+
+//notacao literal de objetos
+const prod2 = {
+ nome: 'camisa polo',
+ preco: 79.90,
+}
+//json é uma estrutura de texte e não um objeto
+//java Script object notation =  json
+'{"nome": "camisa polo","preco": 79.90,}'
+console.log(prod2)
