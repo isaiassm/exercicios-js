e221fcdb6671cfb3ede68e7089d60945e7aa7607
isaiassm <isaias1113@outlook.com>
2018-09-14 19:49:54 -0300

    aplicando var em Js, e entendendo o tratamento dos blocos de acordo com a variavel e funcao

---
+++ fundamentos/UsandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'sera?'
+            }
+        }
+    }
+}
+
+console.log(sera)
+//variavel é visivel mesmo estando fora do bloco
+
+function teste(){
+    var local = 123
+}
+teste()
+console.log(local)
+//variavel criada dentro de funcao só é visivel dentro do seu proprio bloco
