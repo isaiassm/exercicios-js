6f74c25179c98d6a9954c2215ebc1faf798fdb08
isaiassm <isaias1113@outlook.com>
2018-09-17 23:49:51 -0300

    Entendendo o conceito de Hoisting, usado para jogar a variavel para o topo

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,8 @@
+console.log('a = ', a)
+var a = 2
+console.log('a = ', a)
+//hoisting, metodo usado para jogar uma variavel para cima sem ter sido declarado primeiro 
+
+console.log('b = ',b)
+let a = 2
+console.log('b = ',b)
