60fc58d8df767fe90e7f267e93aba0b828cdfb7e
isaiassm <isaias1113@outlook.com>
2018-09-28 17:07:25 -0300

    iniciando modulo de funcao

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,33 @@
+//função em js é First-class object (citizens)
+
+//hifther - order functiom
+
+//criar de forma literal
+function fun1 (){
+}
+
+//armazenar uma funcap em uma variavel
+const fun2 = function () {}
+
+//armazenar em um array
+const array = [function (a , b){ return a + b}, fun1 , fun2]
+console.log(array[0](2,3))
+
+//armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function (){return 'opa'}
+console.log(obj.falar())
+
+//passar funcao como parametro
+function run(fun){
+    fun()
+}
+run(function (){console.log('executando...')})
+
+//uma funcao pode retornar  / conter uma funcao 
+function soma (a, b){
+    return function (c){
+        console.log(a + b + c)
+    }
+}
+soma(2,3)(4)
