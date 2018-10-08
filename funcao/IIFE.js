055adf9218be1393986b23844db13ef119b70e79
isaiassm <isaias1113@outlook.com>
2018-10-07 22:19:50 -0300

    forma de fugir do escopo global

---
+++ funcao/IIFE.js
@@ -0,0 +1,8 @@
+// iife -> imediately invoked function expression
+
+(function(){
+    console.log('sera executado na hora!')
+    console.log('foge do escopo mais abrangente!')
+})()
+
+//para fugir do escopo global (window)
